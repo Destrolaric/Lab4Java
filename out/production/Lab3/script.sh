828ebc764e105594db3e90ca367374837aafb981
Salemar <fireshowel@gmail.com>
2020-05-27 10:38:22 +0300

    added tests launch script

--- out/production/Lab3/script.sh
+++ out/production/Lab3/script.sh
@@ -1,1 +1,2 @@
- /usr/lib/jvm/java-11-openjdk/bin/java -cp . -p . -m info.kgeorgiy.java.advanced.implementor jar-interface com.company.JarImplementor
+ java -cp . -p . -m info.kgeorgiy.java.advanced.implementor interface com.company.Implementor
+ java -cp . -p . -m info.kgeorgiy.java.advanced.implementor jar-interface com.company.JarImplementor
